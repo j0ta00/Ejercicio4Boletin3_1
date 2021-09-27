e0343593d3294912465a26a04879f413299af487
j0ta00 <rypperx@gmail.com>
2021-09-27 20:11:53 +0200

    Initial commit

---
+++ app/src/main/java/com/example/juanjomz/ejercicios3_1/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.juanjomz.ejercicios3_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn1,btn2;
+    private ImageView img;
+    private int idImages[]={R.drawable.descarga,R.drawable.p};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img=(ImageView)findViewById(R.id.imageView);
+        img.setImageResource(R.drawable.descarga);
+        img.setTag("descarga");
+        btn1=findViewById(R.id.button);
+        btn1.setOnClickListener(x->cambiarImagen());
+        btn2=findViewById(R.id.button2);
+        btn2.setOnClickListener(x->cambiarImagen());
+    }
+
+    public void cambiarImagen(){
+        if (img.getTag() == "descarga") {
+            img.setImageResource(R.drawable.p);
+            img.setTag("p");
+        } else {
+            img.setImageResource(R.drawable.descarga);
+            img.setTag("descarga");
+        }
+
+    }
+}
